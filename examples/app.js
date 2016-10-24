0a0003b89dd3c74f239f057701528110c97c5bec
Alessio <repea@hotmail.it>
2016-10-25 00:49:55 +0200

    added lib/ and examples/ folders

--- examples/app.js
+++ examples/app.js
@@ -4,7 +4,7 @@
 
 // get data
 var sampleData = require('./data/data.js');
-var getClusters = require('./Clusters.js');
+var getClusters = require('../lib/Clusters.js');
 var fs = require('fs');
 
 var clusterConfig = {
@@ -35,14 +35,14 @@ var convertData = (oData) => {
 var init = () => {
     var convertedData = convertData(sampleData, clusterConfig);
 
-    /*var json2 = JSON.stringify(convertedData);
+    var json2 = JSON.stringify(convertedData);
     fs.writeFile("./convertedData.json", json2, (err) => {
         if (err) {
             return console.log(err);
         }
 
         console.log("The file was saved!");
-    });*/
+    });
 
     var clusters = getClusters(convertedData);
     var json = JSON.stringify(clusters);
